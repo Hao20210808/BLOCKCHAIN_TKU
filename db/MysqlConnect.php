823fc6a3c1255e4aba465f3df5652c7441d98188
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-10-09 10:45:09 +0800

    Create MysqlConnect.php

---
+++ db/MysqlConnect.php
@@ -0,0 +1,19 @@
+<?php
+  $host = 'localhost';
+  $dbuser = 'root';
+  $dbpw = '';
+  $dbname = 'POPs';
+
+  $link = mysqli_connect($host, $dbuser, $dbpw, $dbname);
+  if ($link) {
+    //( value > 0 ) => 代表連線成功
+    //codes => UTF-8
+    
+    mysql_query($link, "SET NAMES utf8")
+    echo "connected success";
+  } else {
+    //fail, return 'error'
+    //mysql_connect_error();
+    echo 'connected fail :<br/>'mysqli_connect_error();
+  }
+?>
