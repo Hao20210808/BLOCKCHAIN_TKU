0fe10056200fbd63c273d708d298a1cb8ae4b286
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-10-28 09:49:41 +0800

    Create footer.php

---
+++ Server/footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p class="text-center">
+          <?php 
+            echo "&copy;" . date("Y") . "  by.Project of practice course  " 
+          ?>
+          Will.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+Footer
+© 2022 GitHub, Inc.
