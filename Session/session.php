4690ac229689f60aea6a73304bda4506d956e558
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-11-09 09:17:06 +0800

    Delete Session directory

--- Session/session.php
+++
@@ -1,7 +0,0 @@
-<?php
- require_once '/Mysql_DB/MysqlConnect.php';
-
- session_start();
- $_SESSION['UserName'] = '$user_name';
- echo '$_SESSION["UserName"]' . $_SESSION['UserName'];
-?>
--- Session/session_use.php
+++
@@ -1,4 +0,0 @@
-<?php
-  session_start();
-  echo '$_SESSION["UserName"]' . $_SESSION["UserName"];
-?>
