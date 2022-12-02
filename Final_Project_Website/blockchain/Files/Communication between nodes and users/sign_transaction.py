11613533f30d89feadb7636de8e52d30bb3295f8
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-12-02 10:02:46 +0800

    Create sign_transaction.py

---
+++ Final_Project_Website/blockchain/Files/Communication between nodes and users/sign_transaction.py
@@ -0,0 +1,18 @@
+def transaction_to_string(transaction):
+    transaction_dict = {
+        'sender': str(transaction.sender),
+        'receiver': str(transaction.receiver),
+        'amounts': transaction.amounts,
+        'fee': transaction.fee,
+        'message': transaction.message
+    }
+    return str(transaction_dict)
+
+def sign_transaction(transaction, private):
+    private_key = '-----BEGIN RSA PRIVATE KEY-----\n'
+    private_key += private
+    private_key += '\n-----END RSA PRIVATE KEY-----\n'
+    private_key_pkcs = rsa.PrivateKey.load_pkcs1(private_key.encode('utf-8'))
+    transaction_str = transaction_to_string(transaction)
+    signature = rsa.sign(transaction_str.encode('utf-8'), private_key_pkcs, 'SHA-1')
+    return signature
