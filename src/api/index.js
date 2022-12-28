1f34a06c63f147368855e0b674c765ac8818c449
HaoZhong <88621562+Hao20210808@users.noreply.github.com>
2022-12-28 11:44:34 +0800

    Delete src directory

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const systemApi = {
-  user: {
-    login: (data) => axios.post('/api/login', data),
-    registry: (data) => axios.post('/api/registry', data),
-  }
-};
